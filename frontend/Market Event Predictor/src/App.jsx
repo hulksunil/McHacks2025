6770533d1439f3d27074031123c21a2754222a1e
Sunil Kublalsingh <sunil@centrallshop.com>
2025-01-25 17:52:25 -0500

    tried to do a thing with the scichart but couldn't get it to load

--- frontend/Market Event Predictor/src/App.jsx
+++ frontend/Market Event Predictor/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { SciChartReact } from "scichart-react";
+import {
+  SweepAnimation,
+  SciChartJsNavyTheme,
+  NumberRange,
+  EAxisType,
+  EChart2DModifierType,
+  ESeriesType,
+  EPointMarkerType,
+} from "scichart";
+
+const chartConfig = {
+  surface: {
+    theme: new SciChartJsNavyTheme(),
+    title: "SciChart.js First Chart",
+    titleStyle: { fontSize: 22 },
+  },
+  // Create an XAxis and YAxis with growBy padding
+  xAxes: [
+    {
+      type: EAxisType.NumericAxis,
+      options: {
+        axisTitle: "X Axis",
+        growBy: new NumberRange(0.1, 0.1),
+      },
+    },
+  ],
+  yAxes: [
+    {
+      type: EAxisType.NumericAxis,
+      options: {
+        axisTitle: "Y Axis",
+        growBy: new NumberRange(0.1, 0.1),
+      },
+    },
+  ],
+  // Create a line series with some initial data
+  series: [
+    {
+      type: ESeriesType.LineSeries,
+      xyData: {
+        xValues: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        yValues: [
+          0, 0.0998, 0.1986, 0.2955, 0.3894, 0.4794, 0.5646, 0.6442, 0.7173,
+          0.7833,
+        ],
+      },
+      options: {
+        stroke: "steelblue",
+        strokeThickness: 3,
+        pointMarker: {
+          type: EPointMarkerType.Ellipse,
+          options: {
+            width: 11,
+            height: 11,
+            fill: "#fff",
+          },
+        },
+        animation: new SweepAnimation({
+          duration: 300,
+          fadeEffect: true,
+        }),
+      },
+    },
+  ],
+  // Add some interaction modifiers to show zooming and panning
+  modifiers: [
+    { type: EChart2DModifierType.MouseWheelZoom },
+    {
+      type: EChart2DModifierType.ZoomPan,
+      options: { enableZoom: true },
+    },
+    { type: EChart2DModifierType.ZoomExtents },
+  ],
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  // LICENSING
+  // Commercial licenses set your license code here
+  // Purchased license keys can be viewed at https://www.scichart.com/profile
+  // How-to steps at https://www.scichart.com/licensing-scichart-js/
+  // SciChartSurface.setRuntimeLicenseKey("YOUR_RUNTIME_KEY");
+
+  // to use WebAssembly and Data files from CDN instead of the same origin
+  // SciChartSurface.loadWasmFromCDN();
+
+  // Note: for both licensing and WASM configurations - make sure they are set on the client side.
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className="App-header">
+        <h1>SciChart.js with React hello world!</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          In this example we setup webpack, react and use scichart +
+          scichart-react to create a simple chart with one X and Y axis
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+      <SciChartReact config={chartConfig} style={{ maxWidth: 900 }} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/Market Event Predictor/vite.config.js
+++ frontend/Market Event Predictor/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), viteStaticCopy({
+    targets: [
+      { src: 'src/index.html', dest: '' },
+      { src: 'node_modules/scichart/_wasm/scichart2d.data', dest: '' },
+      { src: 'node_modules/scichart/_wasm/scichart2d.wasm', dest: '' },
+      // Optional, if using 3D charts
+      { src: 'node_modules/scichart/_wasm/scichart3d.data', dest: '' },
+      { src: 'node_modules/scichart/_wasm/scichart3d.wasm', dest: '' },
+    ],
+  })]
+
 })
