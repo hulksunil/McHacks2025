46c351cec20649f5ea5c9c41e03e7d67080ce885
Sunil Kublalsingh <sunil@centrallshop.com>
2025-01-26 04:29:05 -0500

    small changes

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 from ..data.preprocessing import get_stock_data
+from flask_cors import CORS
 
 app = Flask(__name__)  # Initializes the Flask app
+CORS(app)  # Allows Cross-Origin Resource Sharing (CORS) for the app
+
 
 @app.route('/api/stock_data', methods=['POST'])
 def stock_data():
     data = get_stock_data()
     return jsonify(data, 200)
+
 
 @app.route('/api/stock_data/market_data', methods=['POST'])
 def market_data():
@@ -18,6 +22,7 @@ def market_data():
 def trade_data():
     data = get_stock_data()
     return jsonify(data['trade_data'], 200)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
