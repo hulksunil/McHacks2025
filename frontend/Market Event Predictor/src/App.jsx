c6c500fc0b2aa11bd44200cc0933bae1ba0ddd0c
Sunil Kublalsingh <sunil@centrallshop.com>
2025-01-26 02:17:07 -0500

    did a bunch of changes and loaded data from the backend into the frontend. Need to moderate the amount of data parsed at a time

--- frontend/Market Event Predictor/src/App.jsx
+++ frontend/Market Event Predictor/src/App.jsx
@@ -3,96 +3,135 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const FirstRequest = () => {};
+const Loader = ({ load }) => {
+  return (
 
-export { FirstRequest };
-
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      type: "line",
-      label: "Stock Price",
-      data: [0, 50, 100, 150, 200, 250, 300],
-      backgroundColor: "rgb(25, 25, 192)",
-      borderColor: "rgb(75, 192, 192)",
-      pointBorderColor: "rgb(75, 100, 192)",
-      fill: true,
-      tension: 0.4,
-    },
-
-    {
-      type: "line",
-      label: "Bid Volume",
-      data: [1, 11, 80, 50, 30, 25, 55],
-      backgroundColor: "rgb(18, 230, 89)",
-      borderColor: "rgb(75, 192, 192)",
-      pointBorderColor: "rgb(5, 100, 10)",
-      fill: true,
-      tension: 0.4,
-    },
-
-    {
-      type: "line",
-      label: "Bid Price",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: "rgb(25, 25, 192)",
-      borderColor: "rgb(75, 192, 192)",
-      pointBorderColor: "rgb(75, 100, 192)",
-      fill: true,
-      tension: 0.4,
-    },
-
-    {
-      type: "line",
-      label: "Ask Volume",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: "rgb(25, 25, 192)",
-      borderColor: "rgb(75, 192, 192)",
-      pointBorderColor: "rgb(75, 100, 192)",
-      fill: true,
-      tension: 0.4,
-    },
-
-    {
-      type: "line",
-      label: "Ask Price",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: "rgb(25, 25, 192)",
-      borderColor: "rgb(75, 192, 192)",
-      pointBorderColor: "rgb(75, 100, 192)",
-      fill: true,
-      tension: 0.4,
-    },
-  ],
-};
+    <div className="loader">
+      <div className={load ? "loader_icon" : "loader_icon loader_icon_none"}></div>
+    </div>
+  );
+}
 
 const App = () => {
-  const [postState, setPostState] = useState(null);
+
+  const [groupedData, setGroupedData] = useState({
+    timestamps: [],
+    stockPrices: [],
+    bidVolumes: [],
+    bidPrices: [],
+    askVolumes: [],
+    askPrices: [],
+  });
+
+  const [load, updateLoad] = useState(true);
+
+
+  const data = {
+    labels: groupedData.timestamps,
+    datasets: [
+      {
+        type: "line",
+        label: "Stock Price",
+        data: groupedData.stockPrices,
+        backgroundColor: "rgb(25, 25, 192)",
+        borderColor: "rgb(75, 192, 192)",
+        pointBorderColor: "rgb(75, 100, 192)",
+        fill: true,
+        tension: 0.4,
+      },
+
+      {
+        type: "line",
+        label: "Bid Volume",
+        data: groupedData.bidVolumes,
+        backgroundColor: "rgb(18, 230, 89)",
+        borderColor: "rgb(75, 192, 192)",
+        pointBorderColor: "rgb(5, 100, 10)",
+        fill: true,
+        tension: 0.4,
+      },
+
+      {
+        type: "line",
+        label: "Bid Price",
+        data: groupedData.bidPrices,
+        backgroundColor: "rgb(25, 25, 192)",
+        borderColor: "rgb(75, 192, 192)",
+        pointBorderColor: "rgb(75, 100, 192)",
+        fill: true,
+        tension: 0.4,
+      },
+
+      {
+        type: "line",
+        label: "Ask Volume",
+        data: groupedData.askVolumes,
+        backgroundColor: "rgb(25, 25, 192)",
+        borderColor: "rgb(75, 192, 192)",
+        pointBorderColor: "rgb(75, 100, 192)",
+        fill: true,
+        tension: 0.4,
+      },
+
+      {
+        type: "line",
+        label: "Ask Price",
+        data: groupedData.askPrices,
+        backgroundColor: "rgb(25, 25, 192)",
+        borderColor: "rgb(75, 192, 192)",
+        pointBorderColor: "rgb(75, 100, 192)",
+        fill: true,
+        tension: 0.4,
+      },
+    ],
+  };
+
+
 
   const postData = async () => {
     try {
-      const response = await axios.post("http://localhost:5173");
-      const data = response.data;
-      setPostState(data);
-      console.log(data);
+      const response = await axios.post("/api/stock_data");
+      const responseData = response.data[0];
+      const marketData = responseData["A"]["market_data"];
+
+      const groupedData = marketData.reduce((acc, obj) => {
+        for (const key in obj) {
+          if (!acc[key]) {
+            acc[key] = []; // Initialize an array for each key
+          }
+          acc[key].push(obj[key]); // Push the value into the respective array
+        }
+        return acc;
+      }, {});
+
+      console.log(groupedData);
+
+      setGroupedData({
+        timestamps: groupedData["timestamp"],
+        stockPrices: groupedData["stockPrice"],
+        bidVolumes: groupedData["bidVolume"],
+        bidPrices: groupedData["bidPrice"],
+        askVolumes: groupedData["askVolume"],
+        askPrices: groupedData["askPrice"]
+      });
+
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
-    postData();
+    postData().then(() => updateLoad(false));
   }, []);
 
   return (
-    <div className="App">
-      <h1>AVNT</h1>
-      <MyChart data={data} />
-      {postState && (
-        <div>Data from POST request: {JSON.stringify(postState)}</div>
-      )}
-    </div>
+    <>
+      <Loader load={load} />
+      <div className="App">
+        <h1>AVNT</h1>
+        <MyChart data={data} />
+      </div>
+    </>
   );
 };
 
