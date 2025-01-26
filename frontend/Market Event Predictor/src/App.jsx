b9ddb364724f92b7f3966b29f1d5de99e6afae9b
TheObliviouSoul <rpatel1241@gmail.com>
2025-01-25 17:50:02 -1000

    Centered graph along with title, and added labels

--- frontend/Market Event Predictor/src/App.jsx
+++ frontend/Market Event Predictor/src/App.jsx
@@ -1,31 +1,99 @@
-import MyChart from './components/MyChart';
-import './App.css';
+import MyChart from "./components/MyChart";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
+const FirstRequest = () => {};
+
+export { FirstRequest };
 
 const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
   datasets: [
     {
-      type: 'line',
-      label: 'Stock Price',
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: 'rgb(25, 25, 192)',
-      borderColor: 'rgb(75, 192, 192)',
-      pointBorderColor: 'rgb(75, 100, 192)',
+      type: "line",
+      label: "Stock Price",
+      data: [0, 50, 100, 150, 200, 250, 300],
+      backgroundColor: "rgb(25, 25, 192)",
+      borderColor: "rgb(75, 192, 192)",
+      pointBorderColor: "rgb(75, 100, 192)",
       fill: true,
-      tension: 0.4
-    }
-  ]
+      tension: 0.4,
+    },
+
+    {
+      type: "line",
+      label: "Bid Volume",
+      data: [1, 11, 80, 50, 30, 25, 55],
+      backgroundColor: "rgb(18, 230, 89)",
+      borderColor: "rgb(75, 192, 192)",
+      pointBorderColor: "rgb(5, 100, 10)",
+      fill: true,
+      tension: 0.4,
+    },
+
+    {
+      type: "line",
+      label: "Bid Price",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: "rgb(25, 25, 192)",
+      borderColor: "rgb(75, 192, 192)",
+      pointBorderColor: "rgb(75, 100, 192)",
+      fill: true,
+      tension: 0.4,
+    },
+
+    {
+      type: "line",
+      label: "Ask Volume",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: "rgb(25, 25, 192)",
+      borderColor: "rgb(75, 192, 192)",
+      pointBorderColor: "rgb(75, 100, 192)",
+      fill: true,
+      tension: 0.4,
+    },
+
+    {
+      type: "line",
+      label: "Ask Price",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: "rgb(25, 25, 192)",
+      borderColor: "rgb(75, 192, 192)",
+      pointBorderColor: "rgb(75, 100, 192)",
+      fill: true,
+      tension: 0.4,
+    },
+  ],
 };
 
-
 const App = () => {
+  const [postState, setPostState] = useState(null);
+
+  const postData = async () => {
+    try {
+      const response = await axios.post("http://localhost:5173");
+      const data = response.data;
+      setPostState(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    postData();
+  }, []);
 
   return (
     <div className="App">
+      <h1>AVNT</h1>
       <MyChart data={data} />
+      {postState && (
+        <div>Data from POST request: {JSON.stringify(postState)}</div>
+      )}
     </div>
-  )
+  );
 };
 
 export default App;
--- frontend/Market Event Predictor/src/components/MyChart.jsx
+++ frontend/Market Event Predictor/src/components/MyChart.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 import {
   Chart as ChartJS,
   LinearScale,
@@ -8,8 +8,8 @@ import {
   LineElement,
   Legend,
   Tooltip,
-} from 'chart.js';
-import { Chart } from 'react-chartjs-2';
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
 
 ChartJS.register(
   LinearScale,
@@ -21,35 +21,33 @@ ChartJS.register(
   Tooltip
 );
 
-
 const options = {
   responsive: true,
   maintainAspectRatio: false,
   plugins: {
     legend: {
-      display: true
-    }
+      display: true,
+    },
   },
   scales: {
     y: {
-      min: 30,
-      max: 90,
+      min: 0,
+      max: 500,
       ticks: {
-        stepSize: 10
-      }
-    }
-  }
+        stepSize: 10,
+      },
+    },
+  },
 };
 
 const MyChart = ({ data }) => {
   const chartRef = useRef(null);
 
   return (
-    <div className='graph' >
-      <h1>Stock Price</h1>
-      <Chart ref={chartRef} type='bar' data={data} options={options} />
+    <div className="graph">
+      <Chart ref={chartRef} type="bar" data={data} options={options} />
     </div>
-  )
-}
+  );
+};
 
 export default MyChart;
